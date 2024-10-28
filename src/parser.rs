a147c0da2e4ca2f021bb50dd0d174397b3cd66be
Roman Brek <rbrek@yandex-team.ru>
2024-10-29 00:09:39 +0700

    Unit tests

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,6 @@
 use log::{debug, error, trace};
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct Problem {
     /// Amount of variables used in |clauses|.
     pub variables_count: u64,
@@ -8,13 +8,13 @@ pub struct Problem {
     pub clauses: Vec<Clause>,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct Literal {
     pub negated: bool,
     pub idx: u64,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct Clause {
     pub var_idxs: Vec<Literal>,
 }
@@ -40,7 +40,7 @@ pub fn parse_cnf(cnf: &str) -> Result<Problem, String> {
 
     debug!("Problem line: {:?}", problem_str);
 
-    let problem_desc = get_problem_description(problem_str);
+    let problem_desc = get_problem_description(problem_str)?;
 
     let clauses_str = lines.collect::<Vec<&str>>().join("\n");
 
@@ -60,7 +60,7 @@ fn get_problem_body(
 ) -> Result<ProblemBody, String> {
     let mut clauses: Vec<_> = clauses_text
         .split('0')
-        .map(|clause| to_clause(clause))
+        .map(|clause| to_clause(problem_desc, clause))
         .collect::<Result<_, _>>()?;
 
     if clauses
@@ -86,34 +86,210 @@ fn get_problem_body(
     Ok(ProblemBody { clauses })
 }
 
-fn to_clause(clause_str: &str) -> Result<Clause, String> {
-    let maybe_clause: Result<Vec<_>, _> = clause_str
-        .split_whitespace()
-        .map(|x| x.parse::<i64>())
-        .map(|xx| {
-            xx.map(|x| Literal {
+fn to_clause(problem_desc: &ProblemDescription, clause_str: &str) -> Result<Clause, String> {
+    fn parse(problem_desc: &ProblemDescription, str: &str) -> Result<Literal, String> {
+        let result = str
+            .parse::<i64>()
+            .map(|x| Literal {
                 negated: x < 0,
                 idx: x.abs() as u64,
             })
-        })
-        .collect();
-    let mut clause =
-        maybe_clause.map_err(|_| format!("Could not parse integer in clause: {}", clause_str))?;
+            .map_err(|_| format!("Could not parse literal {}", str))?;
 
-    // Remove the last '0'.
-    clause.pop();
+        if result.idx == 0 {
+            return Err(format!("Idx 0 is invalid in {}", str));
+        }
+        if result.idx > problem_desc.variables_count {
+            return Err(format!("Idx {} is out of bounce for problem", result.idx));
+        }
+        Ok(result)
+    }
+
+    let maybe_clause: Result<Vec<_>, _> = clause_str
+        .split_whitespace()
+        .map(|x| parse(problem_desc, x))
+        .collect();
+
+    let clause = maybe_clause.map_err(|original_err| {
+        format!(
+            "Could not parse integer in clause: {}, {}",
+            clause_str, original_err
+        )
+    })?;
+
     Ok(Clause { var_idxs: clause })
 }
 
-fn get_problem_description(as_str: &str) -> ProblemDescription {
-    let words = as_str.split_whitespace().collect::<Vec<&str>>();
-    assert!(words.len() == 4);
-    assert!(words[0] == "p");
-    assert!(words[1] == "cnf");
-    let variables_count: u64 = words[2].parse().expect("Error parsing vars count");
-    let clauses_count: u64 = words[3].parse().expect("Error parsing clauses count");
-    return ProblemDescription {
+fn get_problem_description(problem_desc: &str) -> Result<ProblemDescription, String> {
+    let words = problem_desc.split_whitespace().collect::<Vec<&str>>();
+    if words.len() != 4 {
+        return Err(format!(
+            "Problem description contains unexpected amount of words: {}",
+            words.len()
+        ));
+    }
+    if words[0] != "p" {
+        return Err(format!("Problem does not start with p"));
+    }
+    if words[1] != "cnf" {
+        return Err(format!("Problem does not have 'cnf' as seconds word"));
+    }
+
+    let variables_count: u64 = words[2]
+        .parse()
+        .map_err(|_| "Could not parse variables amount")?;
+    let clauses_count: u64 = words[3]
+        .parse()
+        .map_err(|_| "Could not parse clauses count")?;
+
+    Ok(ProblemDescription {
         clauses_count,
         variables_count,
-    };
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const SIMPLE_CNF: &str = "p cnf 3 4\n\
+                              1 2 3 0\n\
+                             -1 2 3 0\n\
+                              1 -2 3 0\n\
+                              1 2 -3 0";
+
+    const CNF_NO_PROBLEM: &str = "\
+                              1 2 3 0\n\
+                             -1 2 3 0\n\
+                              1 -2 3 0\n\
+                              1 2 -3 0";
+
+    const SIMPLE_CNF_COMMENTS: &str = "c this is a comment\n\
+                              c THIS is a different comment\n\
+                              cp also comment\n\
+                              p cnf 3 4\n\
+                              1 2 3 0\n\
+                             -1 2 3 0\n\
+                              1 -2 3 0\n\
+                              1 2 -3 0";
+
+    const SIMPLE_CNF_ENDS_NEWLINE: &str = "p cnf 3 4\n\
+                              1 2 3 0\n\
+                             -1 2 3 0\n\
+                              1 -2 3 0\n\
+                              1 2 -3 0\n";
+
+    const SIMPLE_CNF_NO_ZERO: &str = "p cnf 3 4\n\
+                              1 2 3 0\n\
+                             -1 2 3 0\n\
+                              1 -2 3 0\n\
+                              1 2 -3";
+
+    const EMPTY_CNF: &str = "p cnf 200 0";
+
+    fn parsed_simple_cnf() -> Problem {
+        Problem {
+            variables_count: 3,
+            clauses: vec![
+                Clause {
+                    var_idxs: vec![
+                        Literal {
+                            negated: false,
+                            idx: 1,
+                        },
+                        Literal {
+                            negated: false,
+                            idx: 2,
+                        },
+                        Literal {
+                            negated: false,
+                            idx: 3,
+                        },
+                    ],
+                },
+                Clause {
+                    var_idxs: vec![
+                        Literal {
+                            negated: true,
+                            idx: 1,
+                        },
+                        Literal {
+                            negated: false,
+                            idx: 2,
+                        },
+                        Literal {
+                            negated: false,
+                            idx: 3,
+                        },
+                    ],
+                },
+                Clause {
+                    var_idxs: vec![
+                        Literal {
+                            negated: false,
+                            idx: 1,
+                        },
+                        Literal {
+                            negated: true,
+                            idx: 2,
+                        },
+                        Literal {
+                            negated: false,
+                            idx: 3,
+                        },
+                    ],
+                },
+                Clause {
+                    var_idxs: vec![
+                        Literal {
+                            negated: false,
+                            idx: 1,
+                        },
+                        Literal {
+                            negated: false,
+                            idx: 2,
+                        },
+                        Literal {
+                            negated: true,
+                            idx: 3,
+                        },
+                    ],
+                },
+            ],
+        }
+    }
+
+    #[test]
+    fn simple_cnf() {
+        assert_eq!(parsed_simple_cnf(), parse_cnf(SIMPLE_CNF).unwrap());
+        assert_eq!(
+            parsed_simple_cnf(),
+            parse_cnf(SIMPLE_CNF_ENDS_NEWLINE).unwrap()
+        );
+    }
+
+    #[test]
+    fn empty_cnf() {
+        let expected = Problem {
+            variables_count: 200,
+            clauses: vec![],
+        };
+        assert_eq!(expected, parse_cnf(EMPTY_CNF).unwrap());
+    }
+
+    #[test]
+    fn no_zero_in_end() {
+        assert_eq!(parsed_simple_cnf(), parse_cnf(SIMPLE_CNF_NO_ZERO).unwrap());
+    }
+
+    #[test]
+    fn has_comments() {
+        assert_eq!(parsed_simple_cnf(), parse_cnf(SIMPLE_CNF_COMMENTS).unwrap());
+    }
+
+    #[test]
+    fn no_problem_error() {
+        let error = parse_cnf(CNF_NO_PROBLEM).unwrap_err();
+        assert_eq!(error, "File should contain a line starting with 'p ...'");
+    }
 }
--- src/solver.rs
+++ src/solver.rs
@@ -1,4 +1,3 @@
-use log::debug;
 use log::trace;
 
 use crate::parser::*;
